ef935fde3d5bc085e688956e42476cbe3e007e53
TTwithTT <puttyo76@gmail.com>
2023-06-14 02:02:45 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_074634) do
+  create_table "blocks", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "index"
+    t.string "name"
+    t.integer "length"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "commits", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.datetime "scheduledDate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
