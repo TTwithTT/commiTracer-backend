23ca5baa1818e68985e746e5ac11da8cdf7b1027
TTwithTT <puttyo76@gmail.com>
2023-06-26 04:42:14 +0000

    fix/ cors setting

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,6 @@ module Backend
   class Application < Rails::Application
     config.load_defaults 7.0
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:3000'
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      end
-    end
     config.generators.test_framework = :rspec
     config.generators.system_tests   = false
     config.generators.helper         = false
