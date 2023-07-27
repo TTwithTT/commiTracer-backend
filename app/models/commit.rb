7939f03ec37f35bfe73db8973ba6bd73e73f50ff
TTwithTT <puttyo76@gmail.com>
2023-07-27 07:02:20 +0000

    add/ User model

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,4 +1,5 @@
 class Commit < ApplicationRecord
+  belogns_to :user
   has_many :blocks
   accepts_nested_attributes_for :blocks
 end
--- db/migrate/20230613074320_create_commits.rb
+++ db/migrate/20230613074320_create_commits.rb
@@ -2,6 +2,7 @@ class CreateCommits < ActiveRecord::Migration[7.0]
   def change
     create_table :commits do |t|
       t.string :title
+      t.integer :user_id
 
       t.timestamps
     end
