8f2c410762d2abb127b276a2cedeee3bd32bfe09
TTwithTT <puttyo76@gmail.com>
2023-08-10 05:10:53 +0000

    fix/ path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "commi-tracer-frontend.vercel.app"
+    origins "commitracer.vercel.app"
 
     resource "*",
       headers: :any,
